c1239cd6df516417e772b853d5cdf4fd3325fe0a
pritam shrestha <safal_pritam@hotmail.com>
2019-09-01 09:33:34 -0700

    Add files via upload

---
+++ shrestha_pritam_DSC510.py
@@ -0,0 +1,6 @@
+# Printing hello world using python
+# i have been using this pycharm since few years so i have 2017 community version.
+# Does it work or do i need to install new one?
+
+x = "hello world"
+print(x)
